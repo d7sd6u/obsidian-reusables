86bac61c548df35e9085dda654c6af4083a71cbf
d7sd6u <58745001+d7sd6u@users.noreply.github.com>
2025-02-24 11:09:52 +0400

    feat: improve view patching utilities

--- src/patchingUtils.ts
+++ src/patchingUtils.ts
@@ -1,4 +1,5 @@
 import { App, Component, View } from "obsidian";
+import { ViewRegistryViewByTypeRecord } from "obsidian-typings/src/obsidian/internals/ViewRegistry/ViewRegistryViewByTypeRecord";
 
 export function hookIntoEmbedCreation(
 	app: App,
@@ -22,19 +23,42 @@ export function hookIntoViewCreation(
 	app: App,
 	cb: (type: string, view: View) => void,
 ) {
-	function patchViewCreator(type: string) {
-		const existing = app.viewRegistry.viewByType[type];
-		if (!existing) return;
-		app.viewRegistry.viewByType[type] = function (...args) {
-			const view = existing.apply(this, args);
-			cb(type, view);
-			return view;
-		};
-	}
 	for (const type of Object.keys(app.viewRegistry.viewByType)) {
-		patchViewCreator(type);
+		patchView(app, type, (view) => {
+			cb(type, view);
+		});
 	}
 	app.viewRegistry.on("view-registered", (registeredType) => {
-		patchViewCreator(registeredType);
+		patchView(app, registeredType, (view) => {
+			cb(registeredType, view);
+		});
+	});
+}
+export function patchView<T extends keyof ViewRegistryViewByTypeRecord>(
+	app: App,
+	type: T,
+	cb: (view: ReturnType<ViewRegistryViewByTypeRecord[T]>) => void,
+) {
+	const existing = app.viewRegistry.viewByType[type];
+	if (!existing) return;
+	app.viewRegistry.viewByType[type] = function (...args) {
+		const view = existing.apply(this, args);
+		cb(view as ReturnType<ViewRegistryViewByTypeRecord[T]>);
+		return view;
+	};
+	iterateAllInstancesOfView(app, type, cb);
+}
+
+export function iterateAllInstancesOfView<
+	T extends keyof ViewRegistryViewByTypeRecord,
+>(
+	app: App,
+	type: T,
+	cb: (view: ReturnType<ViewRegistryViewByTypeRecord[T]>) => void,
+) {
+	app.workspace.iterateAllLeaves((leaf) => {
+		if (leaf.view.getViewType() === type) {
+			cb(leaf.view as ReturnType<ViewRegistryViewByTypeRecord[T]>);
+		}
 	});
 }
